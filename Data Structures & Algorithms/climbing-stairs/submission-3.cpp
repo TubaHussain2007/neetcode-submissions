15369eb75781e53877316ffa0138ba5056006176
Tuba Hussain <hussaintuba12345@gmail.com>
2026-08-21 23:55:05 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,13 @@
+class Solution {
+    private:
+    int fnc(int n, vector<int> &dp) {
+        if(n<=1) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = fnc(n-1, dp) + fnc(n-2, dp);
+    }
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, -1);
+        return fnc(n, dp);
+    }
+};
